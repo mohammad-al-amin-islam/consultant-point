cdafa6d01d6404ba4a6f1a6fcdb0bcfed230c075
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-17 10:12:37 +0600

    header and some component added

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="secondary" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to="/">Consultant Point</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
