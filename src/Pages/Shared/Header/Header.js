1124756565cfeda2df4391875f51deb0016e1386
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-17 18:16:26 +0600

    sign out implemented

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from 'firebase/auth';
+import auth from '../../../firebase.init';
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+
     return (
         <Navbar collapseOnSelect expand="lg" bg="secondary" variant="dark">
             <Container>
@@ -10,9 +15,16 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
                         <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
                         <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+
+                        {
+                            user ?
+                                <button onClick={() => signOut(auth)} className='btn btn-link text-decoration-none text-white'>Sign Out</button>
+                                :
+                                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
