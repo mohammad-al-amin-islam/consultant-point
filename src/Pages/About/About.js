adf31e0a801bcb425557d351da89775a12ef60bf
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-18 15:55:24 +0600

    about added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import myself from '../../images/me1.jpg'
 
 const About = () => {
     return (
-        <div>
-            <h4>This is about</h4>
+        <div className='container my-5 shadow-lg rounded'>
+            <div className="card border-0">
+                <div className="card-body">
+                    <div className='my-3 text-center'>
+                        <img className='' src={myself} alt="" />
+                    </div>
+                    <p className="card-text">To enhance my qualities with the opportunities at hand and build up my experiences further in order to pursue strong engineering and technological skills as a successful Computer Engineer with focused work ethics, an ability to increase my knowledge and contribute to significant software development. To achieve this goal i will put my best with giving no excuses of other problems.And i will try to learn from mistake insha allah and come back stongly.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='text-center'>
+        <div className='text-center my-3'>
             <p >CopyRight &copy; {new Date().getFullYear()} All Rights Reserved by al amin</p>
         </div>
     );
